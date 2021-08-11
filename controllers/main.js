497b29a0c6342706331700354bc3fb54f7858e70
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-11 10:42:36 +0200

    authentication middleware added

--- controllers/main.js
+++ controllers/main.js
@@ -16,22 +16,11 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new CustomAPIError("No token provided", 401);
-  }
-  const token = authHeader.split(" ")[1];
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}!`,
-      secret: `Here is your authorized data ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomAPIError("Not authorized to access this route", 401);
-  }
-  console.log(token);
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}!`,
+    secret: `Here is your authorized data ${luckyNumber}`,
+  });
 };
 
 module.exports = {
