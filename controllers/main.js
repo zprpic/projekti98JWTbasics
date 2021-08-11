36997341a521dc4645e1f76465f0a7c828f63119
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-11 09:45:42 +0200

    Auth header...

--- controllers/main.js
+++ controllers/main.js
@@ -16,6 +16,12 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+  const token = authHeader.split(" ")[1];
+  console.log(token);
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John Doe!`,
