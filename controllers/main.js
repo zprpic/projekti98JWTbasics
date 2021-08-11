cfa1030d1d13c9079ad7e01db9629b5cc9b219e0
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-11 10:30:33 +0200

    Token verification added

--- controllers/main.js
+++ controllers/main.js
@@ -21,12 +21,17 @@ const dashboard = async (req, res) => {
     throw new CustomAPIError("No token provided", 401);
   }
   const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}!`,
+      secret: `Here is your authorized data ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
+  }
   console.log(token);
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: `Hello, John Doe!`,
-    secret: `Here is your authorized data ${luckyNumber}`,
-  });
 };
 
 module.exports = {
